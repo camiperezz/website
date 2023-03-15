975c3dcc96712ccbeb922803718577fbf43875b7
camiperezz <79849556+camiperezz@users.noreply.github.com>
2023-03-14 22:02:25 -0300

    Sass architecture + base styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,13 @@
-import '../App.css';
-import React from 'react';
+import React from "react";
 
 class Home extends React.Component {
-    render() {
-        return (
-            <div className="Home">
-                <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                </header>
-            </div>
-            
-        );
-    }
-
+  render() {
+    return (
+      <div className="home">
+        <h1>Soy la Home Page :D</h1>
+      </div>
+    );
+  }
 }
 
 export default Home;
